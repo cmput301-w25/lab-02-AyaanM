986a5d75db301b2c2c16feffc80033126622b27e
AyaanM <ayaan.merchant08@gmail.com>
2025-01-17 10:06:38 -0700

    started working on button

--- code/app/src/main/java/com/example/listycity/MainActivity.java
+++ code/app/src/main/java/com/example/listycity/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.listycity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,15 +18,22 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
-
     ListView cityList;
     ArrayAdapter<String> cityAdapter;
     ArrayList<String> dataList;
+
+    EditText inputCity;
+    String newCity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inputCity = findViewById(R.id.input_text);
+
+        Button addButton = findViewById(R.id.add);
+        Button deleteButton = findViewById(R.id.delete);
 
         EdgeToEdge.enable(this);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
@@ -33,6 +44,7 @@ public class MainActivity extends AppCompatActivity {
 
         cityList = findViewById(R.id.city_list);
 
+
         String[] cities = {"Edmonton", "Paris", "London", "Ottawa"};
 
         dataList = new ArrayList<>();
@@ -40,6 +52,24 @@ public class MainActivity extends AppCompatActivity {
 
         cityAdapter = new ArrayAdapter<>(this, R.layout.content, dataList);
         cityList.setAdapter(cityAdapter);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                newCity = inputCity.getText().toString();
+
+                if (!newCity.isEmpty()) {
+                    dataList.add(newCity);
+                    cityAdapter.notifyDataSetChanged();
+                    inputCity.setText("");
+                }
+
+            }
+        });
+
+    }
+
+    public void addCity(View view){
 
     }
 }
